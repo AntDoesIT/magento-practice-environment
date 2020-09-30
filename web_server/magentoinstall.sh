c1ee172be1e3604a929a5a07142266b6fd80d424
Antoinette Mkwapatira <antdoesit@gmail.com>
2020-09-30 02:01:09 -0400

    Update typo in magento installtion file

--- web_server/magentoinstall.sh
+++ web_server/magentoinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition=2.4
+composer create-project --repository-url=https://repo.magento.com/magento/project-community-edition=2.4
 php /var/www/html/bin/magento setup:install --base-url=http://localhost/ --db-host=mysql --db-name=magento --db-user=root --db-password=newpass --admin-firstname=Admin --admin-lastname=Admin --admin-email=antdoesit@gmail.com --admin-user=admin --admin-password=test123 --language=en_US --backend-frontname=admin --search-engine=elasticsearch7 --elasticsearch-host=elasticsearch --elasticsearch-port=9200
 php /var/www/html/bin/magento deploy:mode:set developer
 php /var/www/html/bin/magento sampledata:deploy
